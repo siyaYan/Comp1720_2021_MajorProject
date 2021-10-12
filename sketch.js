2cffa61eddda79d6a5233565295905f505483207
Xiran <u7167582@anu.edu.au>
2021-10-12 14:06:26 +0800

    modify

--- sketch.js
+++ sketch.js
@@ -1,3 +1,12 @@
+let object={
+  x:0,
+  y:0,
+}
+let speed={
+  x:10,
+  y:10,
+}
+let i=0
 function preload() {
   // load any assets (images, sounds, etc.) here
 }
@@ -5,13 +14,41 @@ function preload() {
 function setup() {
   // add your setup code here
   createCanvas(windowWidth,windowHeight)
+  object.x=windowWidth/2
+  object.y=windowHeight/2
 }
 
 function draw() {
   // add your draw code here
   background(0)
+  i=random(4)
+  bounce()
   fill(250)
   ellipse(mouseX, mouseY, 100, 100)
+  cube()
+}
+function bounce(){
+  if(object.x>=windowWidth||object.x<=0){
+    speed.x*=-1
+  }
+  object.x+=speed.x
+  if(object.y>=windowHeight||object.y<=0){
+    speed.y*=-1    
+  }
+    object.y+=speed.y
+  
+}
+function cube(){
+  if(i<=1){
+    fill(200)
+  }
+  else if(i<=2){
+    fill(150)
+  }
+  else if(i<=3){
+    fill(50)
+  }
+  ellipse(object.x, object.y, 100, 100)
 }
 
 // when you hit the spacebar, what's currently on the canvas will be saved (as
